514245e8475998b5d42da5af98d309f07dc20b08
john mclaughlin <john.n.mclaughlin@gmail.com>
2018-03-04 17:16:07 -0500

    Linting and pruning

--- src/components/ModuleList.jsx
+++ src/components/ModuleList.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import {List, ListItem} from 'material-ui/List';
-  
-export default class ModuleList extends React.Component {
+import { ListItem } from 'material-ui/List';
 
+export default class ModuleList extends React.Component {
 handleModChange = (val) => {
-    this.props.onSelectModule(val);            
+  this.props.onSelectModule(val);
 }
 
 renderList(modules) {
-    const list = [];
-    for (const k in modules){
-        let val = modules[k];
-        let title = val.title;
-        list.push(<ListItem key={k} primaryText={title} onClick={() => this.handleModChange({val})}/> );
-    }
-    return list;
+  const list = [];
+  for (const k in modules) {
+    const val = modules[k];
+    const title = val.title;
+    list.push(<ListItem key={k} primaryText={title} onClick={() => this.handleModChange({ val })} />);
+  }
+  return list;
 }
 
 render() {
-    let modules = this.props.modules;
-    return (
-        <div className="chapter">
-            { this.renderList(modules) }
-        </div>
-    )
-    }
+  const modules = this.props.modules;
+  return (
+    <div className="chapter">
+      { this.renderList(modules) }
+    </div>
+  );
+}
 }
--- src/components/ProgramMenu.jsx
+++ src/components/ProgramMenu.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import {List, ListItem} from 'material-ui/List';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import Subheader from 'material-ui/Subheader';
+import { List, ListItem } from 'material-ui/List';
 import ModuleList from './ModuleList';
 
-export default class ProgramMenu extends React.Component {
+class ProgramMenu extends React.Component {
 
   render() {
-    
+
     return (
       <div>
         <List>
@@ -33,3 +31,31 @@ export default class ProgramMenu extends React.Component {
     );
   }
 }
+
+// const ProgramMenu = () => {
+//   return (
+//     <div>
+//         <List>
+//           {this.props.lessons.map((lesson, i) => {
+//             var mods = lesson.modules;
+//             if(lesson.week <= this.props.userWeek){
+//             return (
+//               <ListItem
+//                 key={i}
+//                 primaryText={lesson.title}
+//                 // leftIcon={<ActionGrade />}
+//                 initiallyOpen={false}
+//                 primaryTogglesNestedList={true}
+//                 nestedItems={[
+//                   <ModuleList modules={mods} key={i} onSelectModule={this.props.onSelectModule}/>
+//                 ]}
+//               />
+//             )
+//             }
+//           })}
+//         </List>
+//       </div>
+//   );
+// };
+
+export default ProgramMenu;
